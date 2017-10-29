42d34474bf0a52b799c92dfbd8ee79b65525d151
bshap93 <bshap93@gmail.com>
2017-10-29 16:41:50 -0600

    Trying to post to api

--- show-tracker-rails-api/app/controllers/api/v1/my_shows_controller_controller.rb
+++ show-tracker-rails-api/app/controllers/api/v1/my_shows_controller_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::MyShowsControllerController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def create
+    my_show = MyShow.find_or_create_by(id: my_show_params[:id])
+    if my_show
+      response = {
+        id: my_show.id
+      }
+      render json: response, status: 201
+    else
+      render json: {}, status: 401
+    end
   end
 
   def update
@@ -14,5 +24,11 @@ class Api::V1::MyShowsControllerController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def my_show_params
+    params.require(:my_show).permit(:id)
   end
 end
