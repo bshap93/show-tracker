48ca90ad6e31cd877911c15a9a6a63f062801f6d
bshap93 <bshap93@gmail.com>
2017-10-31 13:33:52 -0600

    change routes rails

--- show-tracker-rails-api/config/routes.rb
+++ show-tracker-rails-api/config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json } do
+  namespace :api do
     namespace :v1 do
-      post 'my_shows', :to => 'my_shows_controller#create'
-
-      patch 'my_shows/:show', to: 'my_shows_controller#update'
-
-      delete 'my_shows/:show', :to => 'my_shows_controller#destroy'
-
-      get 'my_shows', :to => 'my_shows_controller#index'
-
-      get 'my_shows/:show', :to => 'my_shows_controller#show'
+      resources :my_shows
     end
   end
 end
